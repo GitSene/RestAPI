af55a708a3e9cb9da672fcb1bab16a7b5d92e71f
BabGit <babek.fr@gmail.com>
2019-05-05 23:47:22 -0400

    RestAssuredFramework

---
+++ restAssuredFramework/src/main/java/restAssured1/restAssured1/App.java
@@ -0,0 +1,13 @@
+package restAssured1.restAssured1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
